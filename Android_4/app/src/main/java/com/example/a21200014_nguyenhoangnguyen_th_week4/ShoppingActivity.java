0825087e775d831963514e559a2b114de28c0394
trungbabaok <trungmuok123@gmail.com>
2025-04-22 20:45:38 +0700

    First Commit

---
+++ Android_4/app/src/main/java/com/example/a21200014_nguyenhoangnguyen_th_week4/ShoppingActivity.java
@@ -0,0 +1,25 @@
+package com.example.a21200014_nguyenhoangnguyen_th_week4;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ShoppingActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.shopping_layout);
+
+        View rootView = findViewById(android.R.id.content);
+        rootView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ShoppingActivity.this, GameActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
+
+
